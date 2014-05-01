f88683c5e27880ac7840730ea4de124ffec11254
V-Roger <roger.virgil@gmail.com>
2014-05-01 11:47:40 +0200

    Tool UI & Structure update
    - Working main nav
    - Structural fixes
    - First tool integration tests

--- dist/tool_ui.js
+++ dist/tool_ui.js
@@ -1,18 +1,71 @@
 $(document).ready(function(){
 
-	$('#transformer_trace').click(function(){
+	$('#nav_traces > li > a').click(function(){
 
-		var vis = $('#toolbox').css('display') == 'block' ? true : false;
+		var id = $(this).attr('id');
 
-		if(vis)
+		$(this).parent().addClass('active');
+		$(this).parent().siblings().removeClass('active');
+
+		var toolbox_vis = $('#toolbox').css('display') == 'block' ? true : false;
+
+		switch (id)
 		{
-			$('#toolbox').toggle();
-			$('#traces').css('height', '90%');
-		}
-		else
-		{
-			$('#toolbox').toggle();
-			$('#traces').css('height', '50%');
+			case 'load_trace' : $('#row_load').siblings('.col-md-10').hide();
+								$('#row_load').show();
+								if(toolbox_vis)
+								{
+									$('#toolbox').hide();
+									$('#traces').css('height', '92%');
+								}
+								break;
+
+			case 'transform_trace' : 	$('#row_transform').siblings('.col-md-10').hide();
+										$('#row_transform').show();
+										if(!toolbox_vis)
+										{
+											$('#toolbox').show();
+											$('#traces').css('height', '50%');
+										}
+									 	break;
+
+			case 'compare_trace' : 	$('#row_compare').siblings('.col-md-10').hide();
+									$('#row_compare').show();
+									if(toolbox_vis)
+									{
+										$('#toolbox').hide();
+										$('#traces').css('height', '92%');
+									}
+								   	break;
+
+			case 'import_trace' : 	$('#row_import').siblings('.col-md-10').hide();
+									$('#row_import').show();
+									if(toolbox_vis)
+									{
+										$('#toolbox').hide();
+										$('#traces').css('height', '92%');
+									}
+									break;
+
+			case 'history' : 	$('#row_history').siblings('.col-md-10').hide();
+								$('#row_history').show();
+								if(toolbox_vis)
+								{
+									$('#toolbox').hide();
+									$('#traces').css('height', '92%');
+								}
+								break;
+
+			case 'download_transformation' : 	$('#row_download').siblings('.col-md-10').hide();
+												$('#row_download').show();
+												if(toolbox_vis)
+												{
+													$('#toolbox').hide();
+													$('#traces').css('height', '92%');
+												}
+												break;
+
+			default : break;
 		}
 	});
 
